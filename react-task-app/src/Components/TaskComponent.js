569b4191432f69b7c1ce87f828d453222697888f
freddyhenao <61300657+freddyhenao@users.noreply.github.com>
2020-11-26 20:11:50 -0500

    api python se agrega lista de tareas
    se garega  a la API
    editar datos en el api

--- react-task-app/src/Components/TaskComponent.js
+++ react-task-app/src/Components/TaskComponent.js
@@ -1,54 +1,95 @@
-//  Este codigo es el encargado de consultar la API -Python y traer los datos para ser renderizados 
+import React, { useEffect, useState } from 'react';
 
-import React, {useEffect, useState} from 'react';
-
-const TasksComponent= () => {
-
-    const [tasks, setTasks] = useState([])
-    const fetchTasks = async() =>{
-          const response = await fetch("http://localhost:5000/listtasks",
-          { 
-              method:'GET',
-              headers:{
-                Accept:'aplication/json',
-                'Content-Type':'aplication/json',               
-              }
-          } 
-          );
-          const dataResponse = await response.json(); 
-          const dataTasks = dataResponse.data.map((item)=>{
-              return <tr key={item.id.toString()}>
-              <td>{item.id}</td>
-              <td>{item.task}</td>
-              <td>{item.date}</td>
-            </tr>
-          });
-          setTasks(dataTasks)
-          
+const TasksComponent = () => {
+    const apiBaseUrl = "http://localhost:4000";
+    const [updateView, setUpdateView] = useState(true);
+    const [tasks, setTasks] = useState([]);
+    const [formData, setFormData] = useState({
+        task: '',
+        date: ''
+    });
+    const handleInputChange = (event) => {
+        console.log(event.target.value);
+        setFormData({
+            ...formData,
+            [event.target.name]:event.target.value
+        });
     }
-    useEffect(()=>{
-          fetchTasks()
-    },[]);
+    const fetchTasks = async () => {
+        const response = await fetch(`${apiBaseUrl}/listtasks`,
+            {
+                method: 'GET',
+                headers: {
+                    Accept: 'aplication/json',
+                    'Content-Type': 'aplication/json',
+                }
+            }
+        );
+        const dataResponse = await response.json();
+        const dataTasks = dataResponse.data.map((item) => {
+            return <tr key={item.id.toString()}>
+                <td>{item.id}</td>
+                <td>{item.task}</td>
+                <td>{item.date}</td>
+            </tr>
+        });
+        setTasks(dataTasks);
+    }
 
-      return(
-      <div className="container" >
-      <table>
-              <thead>
-                <tr>
-                    <th>Id</th>
-                    <th>Task</th>
-                    <th>Date</th>
-                </tr>
-              </thead>
+    const createTask = async (event) => {
+        event.preventDefault();
+        const response = await fetch(`${apiBaseUrl}/createTask`,
+            {
+                method: 'POST',
+                headers: {
+                    Accept: 'aplication/json',
+                    'Content-Type': 'aplication/json',
+                },
+                body: JSON.stringify(formData)
+            }
+        );
+        const dataResponse = await response.json();
+        setUpdateView(!updateView);
+        console.log(dataResponse);
+    }
+    useEffect(() => {
+        fetchTasks();
+    }, [updateView])
+    return (
+        <div className="container">
+            <div onSubmit={createTask} className="row">
+                <form className="col s12">
+                    <div className="row">
+                        <div className="input-field col s12">
+                            <input type="text" name="task" onChange={handleInputChange} />
+                            <label>Task</label>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="input-field col s12">
+                            <input type="text" name="date" onChange={handleInputChange} />
+                            <label >Date</label>
+                        </div>
+                    </div>
+                    <button className="waves-effect waves-light btn-large indigo darken-1">Crear Tarea</button>
+                </form>
+            </div>
+            <p>"task {formData.task}</p>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Task</th>
+                        <th>Date</th>
+                    </tr>
+                </thead>
 
-              <tbody>
-            {/*  se agrega en este punto el codio html para mostrar los datos */}
-              {tasks}
-              </tbody>
+                <tbody>
+                    {tasks}
+                </tbody>
             </table>
-      </div>
-
-  )
-
+        </div>
+    );
 }
-export default TasksComponent;
+
+export default TasksComponent
--- task_app_api_python/app.py
+++ task_app_api_python/app.py
@@ -3,6 +3,7 @@
 from flask import Flask, render_template, request, redirect, url_for, json
 import mysql.connector
 from flask_cors import CORS, cross_origin   
+from flask.json import jsonify
 
 
 
@@ -12,18 +13,18 @@ mydb = mysql.connector.connect(
     passwd = "",
     database = "taskapp"
 )
-
+# permite el acceso de los datos a la base de datos 
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = ['Content-Type']
-
+# convierte los datos obtenidos en formato Json
 def toJson(data):
     tasks = []
     for i in data:
         task = {'id':i[0],'task':i[1],'date':i[2]}
         tasks.append(task)
     return tasks
-
+# consulta por metodo GET "oculto no lo pude ver el usuario " y se adjunta la solisitud de ingreso del metodo @cros
 @app.route('/listtasks', methods=['GET'])
 @cross_origin()
 def index():
@@ -33,6 +34,19 @@ def index():
     result = cur.fetchall()
     print(result)
     return json.dumps({"data": toJson(result)})
-    
+
+#  consulta por metodo POST "lo pude ver el usuario " y se adjunta la solisitud de ingreso del metodo @cros
+@app.route('/listtasks', methods=['POST'])
+@cross_origin()
+def createTask():
+    data = request.get_json(force=True)
+    sql =f"SELECT INTO tasks (task, date) VALUES ('{data['task']}','{data['date']}')"
+    cur = mydb.cursor()
+    cur.execute(sql)
+    mydb.commit()
+    response = jsonify({'response': 'ok'})
+    return response
+
+#  con esta instruccion se conecta a la direccion ip de la maquina y al puerto en este caso port = 4000
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=4000)
